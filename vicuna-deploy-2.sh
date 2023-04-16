47585f4ee867a233319263a276aff5010c01336f
carlosseda <carlossedagambin@gmail.com>
2023-04-16 14:51:20 +0200

    bash

--- vicuna-deploy-2.sh
+++ vicuna-deploy-2.sh
@@ -6,6 +6,7 @@ apt install -y language-pack-ca
 apt install -y unzip
 conda activate textgen
 pip install -r requirements.txt -q
+pip install -i https://test.pypi.org/simple/ bitsandbytes-cuda113 -q
 python download-model.py TheBloke/vicuna-13B-1.1-GPTQ-4bit-128g
 cd repositories/ 
 cd GPTQ-for-LLaMa
