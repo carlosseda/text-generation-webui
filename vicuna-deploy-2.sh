460aad21e952ba181694f374abb47e1d336c5a4a
carlosseda <carlossedagambin@gmail.com>
2023-04-16 14:36:31 +0200

    bash

---
+++ vicuna-deploy-2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update
+apt install -y build-essential
+apt install -y language-pack-es
+apt install -y language-pack-ca
+apt install -y unzip
+conda activate textgen
+pip install -r requirements.txt -q
+python download-model.py TheBloke/vicuna-13B-1.1-GPTQ-4bit-128g
+cd repositories/ 
+cd GPTQ-for-LLaMa
+pip install -r requirements.txt -q
+cd ../..
+pip install --upgrade pymupdf -q
