3c7150b39ee22be2fe4c43f93b15c4356b714741
carlosseda <carlossedagambin@gmail.com>
2023-04-16 13:54:58 +0200

    bash

--- vicuna-deploy.sh
+++ vicuna-deploy.sh
@@ -5,7 +5,7 @@ apt install -y build-essential
 apt install -y language-pack-es
 apt install -y language-pack-ca
 apt install -y unzip
-conda create -n textgen python=3.10.9
+conda create -y -n textgen python=3.10.9
 conda activate textgen
 pip install -r requirements.txt -q
 python download-model.py TheBloke/vicuna-13B-1.1-GPTQ-4bit-128g
